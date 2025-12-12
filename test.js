238f5b07420c6a84aad62d798ee515dc52fc360f
SiyaM108 <deprominent@gmail.com>
2025-12-12 13:02:22 +0000

    complete test.js

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 // Your code here
+const Employee = require("./employee");
+
+const john = new Employee("John Wick", "Dog Lover");
+
+// ---- sayName after 2 seconds ----
+setTimeout(john.sayName.bind(john), 2000);
+
+// ---- sayOccupation after 3 seconds ----
+setTimeout(john.sayOccupation.bind(john), 3000);
